e1b7f407d2d27f63a11f67974099e2c851ee867b
Paulo Hauck <paulovhe@gmail.com>
2017-09-21 20:08:51 -0300

    Splitting the project into two projects, the lib and the tool.

---
+++ QuartzCronGeneratorTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QuartzCronGeneratorTool
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
